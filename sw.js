8822545fc331c04857e1bb2b59038f3335f66cad
Kapil Choudhari <kcaistudio@gmail.com>
2026-09-03 08:41:21 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE='my-task-manager-v2';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(resp=>{
+    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return resp;
+  }).catch(()=>cached)));
+});
+self.addEventListener('notificationclick',event=>{
+  event.notification.close();
+  event.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(list=>{
+    if(list.length) return list[0].focus();
+    return clients.openWindow('./index.html');
+  }));
+});
